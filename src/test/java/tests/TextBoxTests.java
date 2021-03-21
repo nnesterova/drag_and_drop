da50fc08ad34027649915a9e9cb0dc1c1e02196b
nnesterova <ParolGit1994>
2021-03-21 22:38:14 +0300

    added files

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -1,12 +1,12 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class TextBoxTests {
 
@@ -19,8 +19,15 @@ public class TextBoxTests {
     void successfulFillTest()
     {
         open("https://the-internet.herokuapp.com/drag_and_drop");
-        $("#column-a").dragAndDropTo("#column-b");
-        $("#column-a").shouldHave(text("B"));
-        $("#column-b").shouldHave(text("A"));
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        //способ с action
+        SelenideElement x = $("#column-a");
+        SelenideElement y = $("#column-b");
+        actions().moveToElement(x).click(y).perform();
+        //способ просто командой
+        // $("#column-a").dragAndDropTo("#column-b");
+        //проверка, но почему то если запускать вместе с action, то тест падает, так что я не уверена что action тогда работает
+        // $("#column-a").shouldHave(text("B"));
+        // $("#column-b").shouldHave(text("A"));
     }
 }
